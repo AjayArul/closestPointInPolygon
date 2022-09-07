bc9d56193daa5c259150be3fa5000bdd9d0b07c0
AjayArul <ajay24arul@gmail.com>
2022-09-07 19:09:57 +0530

    polygons shapes render

--- index.ts
+++ index.ts
@@ -5,9 +5,14 @@ import './style.css';
 var canvas = document.getElementById('myCanvas')! as HTMLCanvasElement;
 // appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
 
+interface Point {
+  x: number;
+  y: number;
+}
+
 var ctx = canvas.getContext('2d')!;
 canvas.width = 400;
-canvas.height = 500;
+canvas.height = 400;
 
 const polygons = [
   [
@@ -37,8 +42,64 @@ const polygons = [
   ],
 ];
 
-function render() {
+let point: Point | undefined;
+var hover = false;
+
+function polygonsRender() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  // Draw polygons
+  polygons.forEach(function (poly) {
+    ctx.fillStyle = 'rgba(255,255,255,.1)';
+    ctx.strokeStyle = 'rgba(255,255,255,.3)';
+    ctx.lineWidth = 2;
+    ctx.beginPath();
+    // ctx.moveTo(poly[0].x, poly[0].y);
+    poly.forEach(function (vertex) {
+      ctx.lineTo(vertex.x, vertex.y);
+    });
+    ctx.closePath();
+    ctx.fill();
+    ctx.stroke();
+
+    // Draw the closest point on each polygon
+  });
+
+  // Draw the point
+  if (point) {
+    ctx.beginPath();
+    ctx.arc(point.x, point.y, 10, 0, 2 * Math.PI, false);
+    ctx.fillStyle = '#000';
+    // ctx.fill();
+    // ctx.lineWidth = 5;
+    ctx.strokeStyle = 'rgba(255,255,255,.3)';
+    ctx.stroke();
+  }
 }
 
-render();
+polygonsRender();
+
+function mouseMoveHandler(e: MouseEvent) {
+  // Get the current mouse position
+  const touchCoords = { x: e.clientX, y: e.clientY };
+  const { x: Ox, y: Oy } = (
+    e.target as HTMLCanvasElement
+  ).getBoundingClientRect();
+  point = {
+    x: Math.floor(touchCoords.x - Ox),
+    y: Math.floor(touchCoords.y - Oy),
+  };
+  polygonsRender();
+}
+
+canvas.onmousemove = function (e) {
+  mouseMoveHandler(e);
+};
+
+function closestPointInPolygon(poly: Point[], pos: Point): Point {
+  // This function should return the closest point to
+
+  // "pos" that is inside the polygon defined by "poly"
+  
+
+}
